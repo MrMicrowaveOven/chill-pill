3ac38f7d4ba5cc96bf18c7631e0770f348262447
mrmicrowaveoven <benj@zagorski.com>
2024-08-26 19:50:18 -0500

    Updated spacing for emptyTrashIcon

--- public/components/PillManager.tsx
+++ public/components/PillManager.tsx
@@ -115,7 +115,7 @@ const styles = StyleSheet.create({
     emptyTrashButton: {
         position: 'absolute',
         bottom: 20,
-        right: 20,
+        right: -5,
         width: 75,
         height: 75,
     },
