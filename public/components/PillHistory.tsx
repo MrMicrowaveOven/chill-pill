31ab68293f22da5c5573174580228f6798263ea5
mrmicrowaveoven <benj@zagorski.com>
2024-09-10 18:35:10 -0400

    Can now send emails

--- public/components/PillHistory.tsx
+++ public/components/PillHistory.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import type {PropsWithChildren} from 'react';
 import { Pill, SessionDate } from '../types'
 import Button from "./Button";
 import DropDownPicker from "react-native-dropdown-picker";
+import { MMKVLoader, useMMKVStorage } from 'react-native-mmkv-storage';
+const storage = new MMKVLoader().initialize();
+import emailjs from '@emailjs/react-native';
+import {REACT_APP_EMAIL_SERVICE_ID, REACT_APP_EMAIL_TEMPLATE_ID, REACT_APP_EMAIL_USER_ID} from '@env';
 
 type PillHistoryProps = PropsWithChildren<{
     pillHistory: SessionDate[];
@@ -18,6 +22,9 @@ const PillHistory = ({pillHistory, historyreverse, pillList}: PillHistoryProps) 
         return { label: `${pill.name}: ${pill.dosage}${pill.unit}`, value: index }
     }))
     const [filterValue, setFilterValue] = useState<number|null>(null)
+    const [emailEditable, setEmailEditable] = useState<boolean>(false)
+    const emailEdit = useRef<TextInput>(null)
+    const [userEmail, setUserEmail] = useMMKVStorage<string>('userEmail', storage, '')
 
     const downloadHistory = () => {
         console.log(pillHistory)
@@ -35,6 +42,29 @@ const PillHistory = ({pillHistory, historyreverse, pillList}: PillHistoryProps) 
             return includesFilteringPill
         })
         : pillHistory
+
+    useEffect(() => {
+        if(emailEditable) emailEdit.current?.focus()},
+        [emailEditable]
+    )
+
+    const sendEmail = () => {
+        emailjs.send(
+            REACT_APP_EMAIL_SERVICE_ID,
+            REACT_APP_EMAIL_TEMPLATE_ID,
+            {
+                user_email: userEmail,
+                pillHistory: pillHistory
+            },
+            { publicKey: REACT_APP_EMAIL_USER_ID },
+          )
+            .then((response) => {
+              console.log('SUCCESS!', response.status, response.text);
+            })
+            .catch((err) => {
+              console.log('FAILED...', err);
+            });
+    }
 
     return (
         <View style={styles.window}>
@@ -94,6 +124,24 @@ const PillHistory = ({pillHistory, historyreverse, pillList}: PillHistoryProps) 
             </View>
             <View style={styles.downloadHistoryButton}>
                 <Button title="Download Pill History" onPress={() => downloadHistory()}></Button>
+            </View>
+            <View style={styles.emailSection}>
+                <TouchableOpacity onPress={() => setEmailEditable(!emailEditable)}>
+                    <Text style={styles.editEmailButton}>{emailEditable ? 'save' : 'edit'}</Text>
+                </TouchableOpacity>
+                <View style={styles.emailInputBorder}>
+                    <TextInput
+                        style={[styles.emailInput, {color: emailEditable ? 'black' : 'gray'}]}
+                        editable={emailEditable} ref={emailEdit}
+                        value={userEmail}
+                        onChangeText={(value) => setUserEmail(value)}
+                        inputMode='email'
+                        keyboardType='email-address'
+                    />
+                </View>
+                <TouchableOpacity onPress={() => sendEmail()} style={styles.sendEmailButton}>
+                    <Image style={styles.sendEmailImage} source={require("../images/sendEmailIcon.png")}/>
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -155,6 +203,37 @@ const styles = StyleSheet.create({
         color: "black",
         fontSize: 20
     },
+    emailSection: {
+        position: 'absolute',
+        bottom: 10,
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'gray'
+    },
+    editEmailButton: {
+        fontSize: 20,
+        marginLeft: 10,
+        color: 'blue'
+    },
+    emailInputBorder: {
+        flex: 1,
+        margin: 20,
+        borderColor: "black",
+        borderWidth: 1,
+        borderStyle: "solid",
+    },
+    emailInput: {
+        width: '100%',
+        fontSize: 30,
+        backgroundColor: "white",
+    },
+    sendEmailButton: {
+        marginRight: 10,
+    },
+    sendEmailImage: {},
     downloadHistoryButton: {
         position: 'absolute',
         bottom: 10,
---
+++ types/env.d.ts
@@ -0,0 +1,5 @@
+declare module '@env' {
+    export const REACT_APP_EMAIL_USER_ID: string;
+    export const REACT_APP_EMAIL_SERVICE_ID: string;
+    export const REACT_APP_EMAIL_TEMPLATE_ID: string;
+}
